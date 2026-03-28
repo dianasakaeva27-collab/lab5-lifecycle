5300824e2a8cfcd5b8d656b7e08790d27b4ad876
dianasakaeva27-collab <dianasakaeva27@gmail.com>
2026-03-29 01:23:30 +0500

    04_returning_objects.cpp

--- 04_returning_objects.cpp
+++ 04_returning_objects.cpp
@@ -1,111 +1,127 @@
 #include <iostream>
 using namespace std;
 
-// ========== BASE CLASS ==========
+// ========== БАЗОВЫЙ КЛАСС ==========
 class Base {
 public:
+    // Конструктор по умолчанию
     Base() {
-        cout << "Base::Base() constructor" << endl;
+        cout << "Base::Base() конструктор" << endl;
     }
     
+    // Конструктор копирования
     Base(const Base& other) {
-        cout << "Base::Base(const Base&) COPY constructor" << endl;
+        cout << "Base::Base(const Base&) КОНСТРУКТОР КОПИРОВАНИЯ" << endl;
     }
     
+    // Виртуальный деструктор
     virtual ~Base() {
-        cout << "Base::~Base() destructor" << endl;
+        cout << "Base::~Base() деструктор" << endl;
     }
     
+    // Виртуальный метод вывода
     virtual void print() {
         cout << "Base::print()" << endl;
     }
 };
 
-// ========== DERIVED CLASS ==========
+// ========== КЛАСС-ПОТОМОК ==========
 class Desc : public Base {
 public:
+    // Конструктор потомка
     Desc() {
-        cout << "Desc::Desc() constructor" << endl;
+        cout << "Desc::Desc() конструктор" << endl;
     }
     
+    // Конструктор копирования потомка
     Desc(const Desc& other) : Base(other) {
-        cout << "Desc::Desc(const Desc&) COPY constructor" << endl;
+        cout << "Desc::Desc(const Desc&) КОНСТРУКТОР КОПИРОВАНИЯ" << endl;
     }
     
+    // Деструктор потомка
     ~Desc() {
-        cout << "Desc::~Desc() destructor" << endl;
+        cout << "Desc::~Desc() деструктор" << endl;
     }
     
+    // Переопределение виртуального метода
     void print() override {
         cout << "Desc::print()" << endl;
     }
 };
 
-// ========== FUNCTIONS RETURNING LOCAL OBJECTS (DANGEROUS!) ==========
+// ========== ФУНКЦИИ, ВОЗВРАЩАЮЩИЕ ЛОКАЛЬНЫЕ ОБЪЕКТЫ (ОПАСНО!) ==========
+
+// 1. Возврат по значению (создаётся копия)
 Base func1() {
     Base local;
-    cout << "  func1: returning local object" << endl;
-    return local;  // creates copy
+    cout << "  func1: возвращаем локальный объект" << endl;
+    return local;  // создаётся копия
 }
 
+// 2. Возврат указателя на локальный объект (ОПАСНО!)
 Base* func2() {
     Base local;
-    cout << "  func2: returning POINTER to local object" << endl;
-    return &local;  // DANGEROUS! local dies after function exit
+    cout << "  func2: возвращаем УКАЗАТЕЛЬ на локальный объект" << endl;
+    return &local;  // ОПАСНО! local умрёт после выхода из функции
 }
 
+// 3. Возврат ссылки на локальный объект (ОПАСНО!)
 Base& func3() {
     Base local;
-    cout << "  func3: returning REFERENCE to local object" << endl;
-    return local;   // DANGEROUS! local dies after function exit
+    cout << "  func3: возвращаем ССЫЛКУ на локальный объект" << endl;
+    return local;   // ОПАСНО! local умрёт после выхода из функции
 }
 
-// ========== FUNCTIONS RETURNING DYNAMIC OBJECTS ==========
+// ========== ФУНКЦИИ, ВОЗВРАЩАЮЩИЕ ДИНАМИЧЕСКИЕ ОБЪЕКТЫ (ПРАВИЛЬНО) ==========
+
+// 4. Возврат указателя на динамический объект
 Base* func4() {
     Base* ptr = new Base();
-    cout << "  func4: returning pointer to dynamic object" << endl;
-    return ptr;  // OK, must delete later
+    cout << "  func4: возвращаем указатель на динамический объект" << endl;
+    return ptr;  // ПРАВИЛЬНО, нужно потом удалить
 }
 
+// 5. Возврат указателя на динамический объект потомка (полиморфизм)
 Base* func5() {
     Desc* ptr = new Desc();
-    cout << "  func5: returning pointer to dynamic derived object" << endl;
-    return ptr;  // OK, virtual works
+    cout << "  func5: возвращаем указатель на динамический объект потомка" << endl;
+    return ptr;  // ПРАВИЛЬНО, виртуальность работает
 }
 
+// 6. Возврат ссылки на динамический объект
 Base& func6() {
     Base* ptr = new Base();
-    cout << "  func6: returning REFERENCE to dynamic object" << endl;
-    return *ptr;  // OK, must delete later
+    cout << "  func6: возвращаем ССЫЛКУ на динамический объект" << endl;
+    return *ptr;  // ПРАВИЛЬНО, нужно потом удалить
 }
 
 int main() {
-    cout << "========== PROGRAM 4: RETURNING OBJECTS ==========\n\n";
+    cout << "========== ПРОГРАММА 4: ВОЗВРАТ ОБЪЕКТОВ ==========\n\n";
     
-    cout << "--- func1: return local object by value ---\n";
-    Base obj1 = func1();  // OK: creates copy
+    cout << "--- func1: возврат локального объекта по значению ---\n";
+    Base obj1 = func1();  // ПРАВИЛЬНО: создаётся копия
     obj1.print();
     
-    cout << "\n--- func2: return pointer to local object (DANGEROUS!) ---\n";
-    Base* ptr2 = func2();  // ptr2 points to dead object!
-    cout << "  After func2, local object is already destroyed!" << endl;
-    // ptr2->print();  // UNCOMMENT - program will crash!
+    cout << "\n--- func2: возврат указателя на локальный объект (ОПАСНО!) ---\n";
+    Base* ptr2 = func2();  // ptr2 указывает на мёртвый объект!
+    cout << "  После func2 локальный объект уже уничтожен!" << endl;
+    // ptr2->print();  // РАЗКОММЕНТИРУЙ - программа упадёт!
     
-    cout << "\n--- func4: return dynamic object (correct) ---\n";
+    cout << "\n--- func4: возврат динамического объекта (правильно) ---\n";
     Base* ptr4 = func4();
     ptr4->print();
-    delete ptr4;  // must delete!
+    delete ptr4;  // обязательно удалить!
     
-    cout << "\n--- func5: return dynamic derived object ---\n";
+    cout << "\n--- func5: возврат динамического объекта потомка ---\n";
     Base* ptr5 = func5();
-    ptr5->print();  // calls Desc::print() thanks to virtual
-    delete ptr5;    // calls ~Desc() and ~Base()
+    ptr5->print();  // вызывается Desc::print() благодаря виртуальности
+    delete ptr5;    // вызывается ~Desc() и ~Base()
     
-    cout << "\n--- func6: return reference to dynamic object ---\n";
+    cout << "\n--- func6: возврат ссылки на динамический объект ---\n";
     Base& ref6 = func6();
     ref6.print();
-    delete &ref6;   // must delete!
+    delete &ref6;   // обязательно удалить!
     
-    cout << "\n========== END OF PROGRAM ==========\n";
+    cout << "\n========== КОНЕЦ ПРОГРАММЫ ==========\n";
     return 0;
 }
